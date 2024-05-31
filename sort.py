a59e0a47884b9642eafe7e8cd3fdf9b55e6823b4
Bao Le <bple@sturnus.lab>
2024-05-31 18:03:18 -0400

    refine

--- sort.py
+++ sort.py
@@ -3,7 +3,7 @@ import yaml
 import argparse
 import numpy as np
 from pathlib import Path
-
+import matplotlib.pyplot as plt
 import spikeinterface.full as si
 from probeinterface import get_probe
 from probeinterface.plotting import plot_probe
@@ -12,14 +12,17 @@ import warnings
 warnings.simplefilter('ignore')
 
 parser = argparse.ArgumentParser()
-parser.add_argument("recording_folder", help="OpenEphys Recording Folder", type=Path)
-parser.add_argument("output_folder", help="Folder to write sorting results to", type=Path)
-parser.add_argument("probe", help="Probe Config File", type=Path)
-parser.add_argument("sorter", help="Sorter Config File", type=Path)
+parser.add_argument("-r", "recording_folder", help="OpenEphys Recording Folder", type=Path, required=True)
+parser.add_argument("-o", "output_folder", help="Folder to write sorting results to", type=Path, required=True)
+parser.add_argument("-p", "probe", help="Probe Config File", type=Path, required=True)
+parser.add_argument("-s", "sorter", help="Sorter Config File", type=Path, required=True)
 p = parser.parse_args()
 
-with open('./configs/general_cfg.yml') as cfg_f:
+abs_path = os.path.abspath(os.path.dirname(__file__))
+with open(Path(abs_path / 'configs/general_cfg.yml')) as cfg_f:
     cfg = yaml.safe_load(cfg_f)
+with open(Path(abs_path / 'configs/metrics.yml')) as mtrc_f:
+    metrics = yaml.safe_load(mtrc_f)
 
 recording_path = p.recording_folder
 try:
@@ -35,66 +38,79 @@ if probe_config['mode'] == 'import':
 
 try:
     assert p.sorter.is_file()
-    algorithm = p.sorter.stem
+    sorter = p.sorter.stem
+    docker_img = sorter.pop('docker')
 except:
     raise ValueError('Sorter config is not a file.')
-with open(p.sorter) as sorter_file:
+with open(p.sorter) as sorter_file: # import config for sorting algorithm
     sparams = yaml.safe_load(sorter_file)
+if sorter == "kilosort2_5":
+    si.Kilosort2_5Sorter.set_kilosort2_5_path(cfg['sorters'][sorter])
+elif sorter == "waveclus":
+    si.WaveClusSorter.set_waveclus_path(cfg['sorters'][sorter])
+elif sorter == "ironclus":
+    si.IronClustSorter.set_ironclust_path(cfg['sorters'][sorter])
+elif sorter == "kilosort3":
+    si.Kilosort3Sorter. set_kilosort3_path(cfg['sorters'][sorter])
+assert sorter in si.installed_sorters()
 
 base_output = p.output_folder
 base_output.mkdir(parents=True, exist_ok=True)
-preprocess_folder = Path(base_output / 'preprocess')
-sorter_folder = Path(base_output / f'results-{algorithm}')
-waveform_folder = Path(base_output / f'waveforms-{algorithm}')
-phy_folder = Path(base_output / f'phy-{algorithm}')
+sorter_folder = Path(base_output / f'results-{sorter}')
+waveform_folder = Path(base_output / f'waveforms-{sorter}')
+phy_folder = Path(base_output / f'phy-{sorter}')
 
-n_cpus = os.cpu_count()
-n_jobs = n_cpus - 4
-job_kwargs = dict(n_jobs=n_jobs, progress_bar=True)
-si.set_global_job_kwargs(**job_kwargs)
+# n_cpus = os.cpu_count()
+# n_jobs = n_cpus - 4
+# job_kwargs = dict(n_jobs=n_jobs, progress_bar=True)
+# si.set_global_job_kwargs(**job_kwargs)
 
 full_raw_rec = si.read_openephys(recording_path)
 raw_rec = full_raw_rec.set_probe(probe)
 recording_f = si.bandpass_filter(raw_rec, freq_min=cfg['filter']['freq_min'], freq_max=cfg['filter']['freq_max'])
 recording_cmr = si.common_reference(recording_f, reference='global', operator='median')
-if preprocess_folder.is_dir():
-    recording_saved = si.load_extractor(preprocess_folder)
-else:
-    recording_saved = recording_f.save(folder=preprocess_folder, overwrite=True)
-    print(f"Frequency Filter & Common Reference Applied. Preprocessed Recording saved to {preprocess_folder} ")
-    print(f'Saved channels ids:\n{recording_saved.get_channel_ids()}')
 
-print(f'Begin Sorting. Sorting algorithm: {algorithm}. Use docker image: {cfg["docker"]}. Manual Parameters: {sparams}')
-sorter_params = si.get_default_sorter_params(algorithm)
+print(f'Begin Sorting. Sorting algorithm: {sorter}. Use docker image: {docker_img}. Manual Parameters: {sparams}')
+sorter_params = si.get_default_sorter_params(sorter)
 sorter_params.update(sparams)
-sorting_data = si.run_sorter(sorter_name=algorithm,
-                             recording=recording_saved,
+sorting_data = si.run_sorter(sorter_name=sorter,
+                             recording=recording_cmr,
                              remove_existing_folder=True,
                              output_folder=sorter_folder,
-                             docker_image=cfg['docker'],
+                             docker_image=docker_img,
                              verbose=True,
                              **sorter_params)
 
 print("Sorting completed. Extracting waveforms.")
-we = si.extract_waveforms(recording_saved,
+we = si.extract_waveforms(recording_cmr,
                           sorting_data,
                           folder = waveform_folder,
                           sparse = False, overwrite=True)
 sparsity = si.compute_sparsity(we, method=cfg['sparsity']['method'], radius_um=cfg['sparsity']['radius_um'])
-we = si.extract_waveforms(recording_saved,
+we = si.extract_waveforms(recording_cmr,
                           sorting_data,
                           folder = waveform_folder,
                           sparsity = sparsity,
                           overwrite=True)
 print("Calculating pca.")
-pc = si.compute_principal_components(we,
+_ = si.compute_principal_components(we,
                                      mode=cfg['pca']['mode'],
                                      n_components=cfg['pca']['n_components'],
                                      load_if_exists=False)
+print("Calculating metrics")
+metric_names = list(metrics.keys())
+default_metrics =  si.get_default_qm_params()
+metric_params = {k: v for k,v in default_metrics.items() if k not in metric_names}
+for k in metric_names:
+    metric_params[k].update(metrics[k])
+_ = si.compute_quality_metrics(we, metric_names=metric_names, verbose=True,  qm_params=metric_params)
+
 print("Saving to phy.")
 si.export_to_phy(we,
                  output_folder=phy_folder,
                  compute_amplitudes=True,
-                 compute_pc_features=True,
+                 # compute_pc_features=True,
                  remove_if_exists=True,
-                 copy_binary=False)
+                 copy_binary=True)
+
+si.plot_rasters(sorting_data, time_range=(0, 60), backend="matplotlib")
